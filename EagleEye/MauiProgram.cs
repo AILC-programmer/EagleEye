923545bbd371d56eec362de0000613c12a2aba4e
arman <terminator_arman@yahoo.com>
2024-07-23 08:58:21 +0330

    Designed MainPage UI.

--- EagleEye/MauiProgram.cs
+++ EagleEye/MauiProgram.cs
@@ -1,6 +1,7 @@
 ﻿using EagleEye.Pages;
 using EagleEye.ViewModels;
 using Microsoft.Extensions.Logging;
+using SkiaSharp.Views.Maui.Controls.Hosting;
 
 namespace EagleEye
 {
@@ -11,6 +12,7 @@ namespace EagleEye
             var builder = MauiApp.CreateBuilder();
             builder
                 .UseMauiApp<App>()
+                .UseSkiaSharp()
                 .ConfigureFonts(fonts =>
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
